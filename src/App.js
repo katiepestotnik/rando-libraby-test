4daceba3c80000c2e562acd1e125cdb38f241828
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-02-20 20:27:57 -0700

    try chris rando npm library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Home from './components/Home'
 import './App.css';
-
+import Rando from "js-rando";
+let rando = new Rando()
 function App() {
+  let num = rando.Random()
+  let colorHex = rando.RandomHexColor()
+  const divStyle = {
+    backgroundColor: colorHex
+  }
+  console.log(typeof(colorHex))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle} className="App">
+      <div>{colorHex}</div>
+      <Home/>
+
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+function Home(props) {
+    return (
+        <div style={{textAlign:"center",  marginTop: '100px'}}>
+            Test
+        </div>
+    );
+}
+
+export default Home;
